2dbb6b52ab60c365b6adec7b90171834c9ace471
Prince kumar Singh <122429010+singhprince9875@users.noreply.github.com>
2023-03-22 22:03:09 +0530

    Add files via upload

---
+++ stringincplusplus.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    string str ="college wallah";
+    cout<<str;
+}*/
+/*#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    //cin>>str;
+    cout<<"enter the string";
+    getline(cin,str);
+    cout<<str;
+}*/
+// to print the ascii value through character
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char ch = 'a';
+    cout<<int(ch)<<endl;
+
+}
